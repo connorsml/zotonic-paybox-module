4be5135dc6c58cee48560ec61b96789643fa5208
Michael Connors <Michael Connors@localhost>
2012-01-20 16:27:51 +0000

    Fetch redirect page from paybox Fetch parameters, and format call to cgi script
     Enter commit message.  Lines beginning with 'HG:' are removed.

--- mod_paybox.erl
+++ mod_paybox.erl
@@ -23,11 +23,49 @@
 -mod_title("PayBox").
 -mod_description("Integration with PayBox Payment Processing. For taking online payments in France.").
 
+-include_lib("zotonic.hrl").
+
 %% interface functions
 -export([
     event/2
 ]).
 
 event({submit, {make_payment, Args}, TriggerId, _TargetId}, Context) ->
+    Host = Context#context.host,
+    HostName = m_config:get_value(site, hostname, Context),
+    Language = m_config:get_value(?MODULE, paybox_language, 'fra', Context),
     {amount, Amount} = proplists:lookup(amount, Args),
+    {order_number, OrderNumber} = proplists:lookup(order_number, Args),
+    {email, Email} = proplists:lookup(email, Args),
+
+    %%% If any of Site, Identifier or Rang is undefiend we can't process the payment
+    Site = m_config:get_value(?MODULE, paybox_site, undefined, Context),
+    Identifier = m_config:get_value(?MODULE, paybox_identifier, undefined, Context),
+    Rang = m_config:get_value(?MODULE, paybox_rang, undefined, Context),
+    
+    case lists:member(undefined, [Site, Identifier, Rang]) of
+        true ->
+            Context;
+        false ->
+            Currency = m_config:get_value(?MODULE, paybox_currency, 978, Context),
+            ReturnUrl = m_config:get_value(?MODULE, paybox_return_url, '/paybox/callback', Context),
+            SuccessUrl = m_config:get_value(?MODULE, paybox_success_url, '/paybox/success', Context),
+            FailedUrl = m_config:get_value(?MODULE, paybox_failed_url, '/paybox/failure', Context),
+            CancelledUrl = m_config:get_value(?MODULE, paybox_cancelled_url, '/paybox/cancelled', Context),
+            
+            Parameters = io_lib:format("PBX_MODE=4 PBX_SITE=~s PBX_RANG=~s PBX_IDENTIFIANT=~s PBX_DEVISE=~s "
+                                       "PBX_PORTEUR='~s' PBX_CMD=~s PBX_TOTAL=~s "
+                                       "PBX_RETOUR='amount:M;error:E;reference:R;transaction:T;status:O'; "
+                                       "PBX_EFFECTUE='~s~s' PBX_REFUSE='~s~s' PBX_ANNULE='~s~s' "
+                                       "PBX_LANGUE=FRA PBX_REPONDRE_A='~s~s' ", [Site, Rang, Identifier, Language, Email, OrderNumber, Amount, HostName, SuccessUrl, HostName, FailedUrl, HostName, CancelledUrl, HostName, ReturnUrl]),
+            Command = io_lib:format("~s ~s", [filename:join([z_utils:lib_dir(priv), "sites", Host, "deps", "modulev2.cgi"]), Parameters]),
+            Result = list_to_binary(os:cmd(Command)),
+            io:format("Result: ~s~n", [Result]),
+            %% find a way to render this page here
+            Context
+    end;
+
+event({postback, {make_payment, Args}, TriggerId, _TargetId}, Context) ->
+    {amount, Amount} = proplists:lookup(amount, Args),
+    io:format("Amount: ~p~n", [Amount]),
     Context.
--- templates/mod_paybox_test.tpl
+++ templates/mod_paybox_test.tpl
@@ -6,10 +6,10 @@
 
     <h1>{_ Run a Test Transaction Using Button _}</h1>
 
-    {% button text="Pay €10"  postback={make_payment amount="1000"} delegate="mod_paybox" %}
+    {% button text="Pay €10"  postback={make_payment amount="1000" email="connorsml@gmail.com" order_number="0001234"} delegate="mod_paybox" %}
 
     <h1>{_ Run a Test Transaction Using a Wire _}</h1>
-    {% wire id="myform" type="submit" postback={make_payment amount="2000"} %}
+    {% wire id="myform" type="submit" postback={make_payment amount="2000" email="connorsml@gmail.com" order_number="0001234"} delegate="mod_paybox" %}
     <form id="myform" method="post" action="postback" delegate="mod_paybox" postback="make_payment">
       <button id="mybutton" type="submit">Pay €20</button>
     </form>
